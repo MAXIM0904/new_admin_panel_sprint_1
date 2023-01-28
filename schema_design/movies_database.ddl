d64159646467dde057c8cc83854ce0c7224ce872
Max <dvdvdvd90@mail.ru>
2023-01-28 18:43:10 +0300

    commit 3 исправление ошибок

--- schema_design/movies_database.ddl
+++ schema_design/movies_database.ddl
@@ -1,8 +1,7 @@
 CREATE SCHEMA IF NOT EXISTS content;
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS content.film_work (
-	id uuid PRIMARY KEY UNIQUE DEFAULT uuid_generate_v4 (),
+	id uuid PRIMARY,
 	title TEXT NOT NULL,
 	description TEXT,
 	creation_date DATE,
@@ -16,7 +15,7 @@ CREATE UNIQUE INDEX IF NOT EXISTS film_work_idx
 	ON content.film_work (creation_date);
 
 CREATE TABLE IF NOT EXISTS content.genre (
-	id uuid PRIMARY KEY UNIQUE DEFAULT uuid_generate_v4 (),
+	id uuid PRIMARY KEY,
 	name TEXT NOT NULL,
 	description TEXT,
 	created timestamp with time zone,
@@ -27,7 +26,7 @@ CREATE UNIQUE INDEX IF NOT EXISTS genre_idx
 	ON content.genre (description);
 
 CREATE TABLE IF NOT EXISTS content.person (
-	id uuid PRIMARY KEY UNIQUE DEFAULT uuid_generate_v4 (),
+	id uuid PRIMARY KEY,
 	full_name TEXT NOT NULL,
 	created timestamp with time zone,
 	modified timestamp with time zone
@@ -37,20 +36,23 @@ CREATE UNIQUE INDEX IF NOT EXISTS person_idx
 	ON content.person (full_name);
 
 CREATE TABLE IF NOT EXISTS content.genre_film_work (
-	id uuid PRIMARY KEY UNIQUE DEFAULT uuid_generate_v4 (),
+	id uuid PRIMARY KEY,
 	genre_id uuid NOT NULL,
 	film_work_id uuid NOT NULL,
 	created timestamp with time zone,
-	FOREIGN key (genre_id) REFERENCES content.genre (id),
-	FOREIGN key (film_work_id) REFERENCES content.film_work (id)
+	FOREIGN key (genre_id) REFERENCES content.genre (id) ON DELETE CASCADE,
+	FOREIGN key (film_work_id) REFERENCES content.film_work (id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS content.person_film_work (
-	id uuid PRIMARY KEY UNIQUE DEFAULT uuid_generate_v4 (),
+	id uuid PRIMARY KEY,
 	person_id uuid NOT NULL,
 	film_work_id uuid NOT NULL,
 	role TEXT,
 	created timestamp with time zone,
-	FOREIGN key (person_id) REFERENCES content.person (id),
-	FOREIGN key (film_work_id) REFERENCES content.film_work (id)
+	FOREIGN key (person_id) REFERENCES content.person (id) ON DELETE CASCADE,
+	FOREIGN key (film_work_id) REFERENCES content.film_work (id) ON DELETE CASCADE
 );
+
+CREATE UNIQUE INDEX IF NOT EXISTS person_film_work_idx
+	ON content.person_film_work (role);
--- sqlite_to_postgres/load_data.py
+++ sqlite_to_postgres/load_data.py
@@ -27,17 +27,6 @@ class FilmWork:
     created: datetime.now()
     modified: datetime.now()
 
-    @staticmethod
-    def save_all_data(pg_connect, data):
-        data = data.__dict__
-        key_columns = data.keys()
-        values = [data[column] for column in key_columns]
-        insert_statement = 'INSERT INTO content.film_work (%s) values %s'
-        with pg_connect.cursor() as cursor_pg:
-            cursor_pg.execute(insert_statement, (AsIs(
-                ','.join(key_columns)), tuple(values)))
-            pg_connect.commit()
-
 
 @dataclass
 class Genre:
@@ -47,17 +36,6 @@ class Genre:
     created: datetime.now()
     modified: datetime.now()
 
-    @staticmethod
-    def save_all_data(pg_connect, data):
-        data = data.__dict__
-        key_columns = data.keys()
-        values = [data[column] for column in key_columns]
-        insert_statement = 'INSERT INTO content.genre (%s) values %s'
-        with pg_connect.cursor() as cursor_pg:
-            cursor_pg.execute(insert_statement, (AsIs(
-                ','.join(key_columns)), tuple(values)))
-            pg_connect.commit()
-
 
 @dataclass
 class Person:
@@ -66,17 +44,6 @@ class Person:
     created: datetime.now()
     modified: datetime.now()
 
-    @staticmethod
-    def save_all_data(pg_connect, data):
-        data = data.__dict__
-        key_columns = data.keys()
-        values = [data[column] for column in key_columns]
-        insert_statement = 'INSERT INTO content.person (%s) values %s'
-        with pg_connect.cursor() as cursor_pg:
-            cursor_pg.execute(insert_statement, (AsIs(
-                ','.join(key_columns)), tuple(values)))
-            pg_connect.commit()
-
 
 @dataclass
 class GenreFilmWork:
@@ -84,17 +51,6 @@ class GenreFilmWork:
     film_work_id: str
     genre_id: str
     created: datetime.now()
-
-    @staticmethod
-    def save_all_data(pg_connect, data):
-        data = data.__dict__
-        key_columns = data.keys()
-        values = [data[column] for column in key_columns]
-        insert_statement = 'INSERT INTO content.genre_film_work (%s) values %s'
-        with pg_connect.cursor() as cursor_pg:
-            cursor_pg.execute(insert_statement, (AsIs(
-                ','.join(key_columns)), tuple(values)))
-            pg_connect.commit()
 
 
 @dataclass
@@ -105,62 +61,91 @@ class PersonFilmWork:
     role: str
     created: datetime.now()
 
-    @staticmethod
-    def save_all_data(pg_connect, data):
-        data = data.__dict__
+
+def save_all_data(extract_data, conn, name_base):
+    for i_extract_data in extract_data:
+        data = i_extract_data.__dict__
         key_columns = data.keys()
         values = [data[column] for column in key_columns]
-        insert_statement = 'INSERT INTO content.person_film_work (%s) values %s'
-        with pg_connect.cursor() as cursor_pg:
+        insert_statement = f'INSERT INTO content.{name_base} (%s) values %s'
+        with conn.cursor() as cursor_pg:
             cursor_pg.execute(insert_statement, (AsIs(
                 ','.join(key_columns)), tuple(values)))
-            pg_connect.commit()
+            conn.commit()
+
+
+def reformat_sqlite_fields(elem: dict) -> dict:
+    if 'created_at' in elem.keys():
+        elem['created'] = elem['created_at']
+        del (elem['created_at'])
+
+    if 'updated_at' in elem.keys():
+        elem['modified'] = elem['updated_at']
+        del (elem['updated_at'])
+
+    if 'file_path' in elem.keys():
+        del (elem['file_path'])
+
+    return elem
+
+
+def _prepare_data(cursor, row: list):
+    data = {}
+    for index, column in enumerate(cursor.description):
+        data[column[0]] = row[index]
+    return data
+
+
+class SQLiteExtractor:
+    def __init__(self, connection, package_limit: int):
+        self.connection = connection
+        self.package_limit = package_limit
+
+    def load_sqlite(self, table: str):
+        try:
+            cursor = self.connection.cursor()
+            cursor.row_factory = _prepare_data
+            try:
+                table_records_query = cursor.execute(f'SELECT * FROM {table}')
+            except sqlite3.Error as e:
+                raise e
+            while True:
+                rows = table_records_query.fetchmany(size=self.package_limit)
+                if not rows:
+                    return
+                yield from rows
+        except Exception as exception:
+            print(exception)
+        finally:
+            cursor.close()
+
+    def format_dataclass_data(self, table, dataclass):
+        data = self.load_sqlite(table)
+        return [dataclass(**reformat_sqlite_fields(elem)) for elem in data]
 
 
 def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
-    """Основной метод загрузки данных из SQLite в Postgres"""
-    cursor = connection.cursor()
-    list_table = ['film_work', 'genre', 'person',
-                  'person_film_work', 'genre_film_work']
-    for i_table in list_table:
-        name_table = ''.join(i_table)
-        cursor.execute(f"Select * FROM {name_table} LIMIT 0")
-        colnames = ', '.join(
-            [desc[0] for desc in cursor.description if desc[0] != 'file_path'])
-        cursor_sql = connection.cursor()
-        print(f"Копирование базы {name_table}")
-        table_records_query = cursor_sql.execute(
-            f"SELECT {colnames} FROM {name_table}")
-        try:
-            table_records = table_records_query.fetchmany(50)
-            while len(table_records) != 0:
-                for data in table_records:
-                    if name_table == 'film_work':
-                        postgres_saver = FilmWork(*data)
-                    elif name_table == 'genre':
-                        postgres_saver = Genre(*data)
-                    elif name_table == 'person':
-                        postgres_saver = Person(*data)
-                    elif name_table == 'genre_film_work':
-                        postgres_saver = GenreFilmWork(*data)
-                    elif name_table == 'person_film_work':
-                        postgres_saver = PersonFilmWork(*data)
-                    postgres_saver.save_all_data(
-                        pg_connect=pg_conn, data=postgres_saver)
-                table_records = table_records_query.fetchmany(50)
-            print(f"Копирование базы {name_table} успешно выполнено")
-        except (Exception, Error) as err:
-            print("Ошибка при работе с базой данных", err)
-        finally:
-            cursor.close()
+    sqlite_extractor = SQLiteExtractor(connection, 50)
+    for base, schema in datatables_list.items():
+        print(f"Копирование таблицы {base}")
+        data = sqlite_extractor.format_dataclass_data(base, schema)
+        save_all_data(data, pg_conn, base)
+        print(f"Копирование таблицы {base} завершено")
+
+
+datatables_list = {
+    'film_work': FilmWork,
+    'person': Person,
+    'genre': Genre,
+    'person_film_work': PersonFilmWork,
+    'genre_film_work': GenreFilmWork,
+}
 
 
 if __name__ == '__main__':
     try:
         dsl = {'dbname': os.environ.get('DATABASE'), 'user': os.environ.get('USER'),
                'password': os.environ.get('PASSWORD'), 'host': os.environ.get('HOST'), 'port': 5432}
-        print('9090')
-        print(dsl)
         with sqlite3.connect('db.sqlite') as sqlite_conn, psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
             load_from_sqlite(sqlite_conn, pg_conn)
         print('Задача копирования базы данных завершена.')
